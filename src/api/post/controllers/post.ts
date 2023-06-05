c26bb7ce1615d0545c393fe592429e368535f4c0
shadi <shadi@thisisdevelopment.nl>
2023-06-05 12:46:59 +0200

    first commit

---
+++ src/api/post/controllers/post.ts
@@ -0,0 +1,40 @@
+"use strict";
+
+/**
+ * post controller
+ */
+
+import { factories } from "@strapi/strapi";
+
+const { createCoreController } = require("@strapi/strapi").factories;
+module.exports = createCoreController("api::post.post", ({ strapi }) => ({
+  async find(ctx) {
+    // Calling the default core action
+    const { data, meta } = await super.find(ctx);
+    const query = strapi.db.query("api::post.post");
+    await Promise.all(
+      data.map(async (item, index) => {
+        const foundItem = await query.findOne({
+          where: {
+            id: item.id,
+          },
+          populate: ["createdBy", "updatedBy"],
+        });
+
+        data[index].attributes.createdBy = {
+          id: foundItem.createdBy.id,
+          firstname: foundItem.createdBy.firstname,
+          lastname: foundItem.createdBy.lastname,
+        };
+        data[index].attributes.updatedBy = {
+          id: foundItem.updatedBy.id,
+          firstname: foundItem.updatedBy.firstname,
+          lastname: foundItem.updatedBy.lastname,
+        };
+      })
+    );
+    return { data, meta };
+  },
+}));
+
+export default factories.createCoreController("api::post.post");
